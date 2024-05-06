2cb76887f012d85460ff2bf55695e293d296506c
Niklas Lausch <niklas.lausch@encoway.de>
2024-05-06 10:29:27 +0200

    added DataLayerXML implements IDataLayer

--- src/main/java/Kaufvertrag/dataLayer/dataAccessObjects/XML/DataLayerXML.java
+++ src/main/java/Kaufvertrag/dataLayer/dataAccessObjects/XML/DataLayerXML.java
@@ -1,4 +1,19 @@
 package Kaufvertrag.dataLayer.dataAccessObjects.XML;
 
-public class DataLayerXML {
+import Kaufvertrag.businessInterfaces.IVertragspartner;
+import Kaufvertrag.businessInterfaces.IWare;
+import Kaufvertrag.dataLayer.businessClasses.Vertragspartner;
+import Kaufvertrag.dataLayer.dataAccessObjects.IDao;
+import Kaufvertrag.dataLayer.dataAccessObjects.IDataLayer;
+
+public class DataLayerXML implements IDataLayer {
+    @Override
+    public IDao<IVertragspartner, String> getDaoVertragspartner() {
+        return null;
+    }
+
+    @Override
+    public IDao<IWare, Long> getDaoWare() {
+        return null;
+    }
 }
